302f992bd6ffdf42b74c3c0c76e4cea0fdd1f8cd
salonishalu2000@gmail.com <salonishalu2000@gmail.com>
2023-11-05 22:26:43 +0530

    Error Page

--- frontend/recipe-app/src/App.js
+++ frontend/recipe-app/src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+// App.js
+import { Routes, Route } from 'react-router-dom';
+import "./css/navbar.css"
+import "./css/login.css"
+import Navbar from './components/Navbar';
+import Signup from './components/Signup';
+import Login from "./components/Login";
+import FetchAllRecipes from './components/FetchAllRecipes';
+import RecipeInfo from './components/RecipeInfo';
+import SavedRecipes from "./components/SavedRecipes";
+import ErrorPage from './components/ErrorPage';
 
-function App() {
-  return (
-    <div>
-      hello !
-    </div>
-  );
-}
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+
+            <Routes>
+
+                <Route path='/' Component={FetchAllRecipes}></Route>
+                <Route path='/signup' Component={Signup}></Route>
+                <Route path='/login' Component={Login}> </Route>
+                <Route path="/recipe-info/:foodId" Component={RecipeInfo}> </Route>
+                <Route path='/saved-recipes' Component={SavedRecipes}> </Route>
+                <Route path="*" Component={ErrorPage} ></Route>
+            </Routes>
+
+        </>
+    );
+};
 
 export default App;
---
+++ frontend/recipe-app/src/components/ErrorPage.js
@@ -0,0 +1,16 @@
+// ErrorPage.js
+
+import React from 'react';
+import "../css/ErrorPage.css";
+function ErrorPage() {
+    return (
+        <>
+            <div className="error-page">
+                <h1>Error</h1>
+                <p>Sorry, something went wrong.</p>
+            </div>
+        </>
+    );
+}
+
+export default ErrorPage;
