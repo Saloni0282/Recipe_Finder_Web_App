28b8f83b88636416c3eb3951d1c89045fe6ee238
salonishalu2000@gmail.com <salonishalu2000@gmail.com>
2023-11-05 20:20:12 +0530

    backend fetch api half done

--- backend/routes/AllRoutes.js
+++ backend/routes/AllRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const AllRoutes = express.Router();
-const { login, signup,home} = require("../controllers/user.controller");
+const { login, signup, home } = require("../controllers/user.controller");
+const { fetchRecipe, recipeDetails, nutritionalInfo}= require("../services/fetch_recipe")
 const {Auth}=require("../middlewares/Auth")
 AllRoutes.post("/login", login);
 AllRoutes.post("/signup", signup);
 AllRoutes.get('/check',Auth,home)
-
+AllRoutes.get("/get", fetchRecipe);
+AllRoutes.get("/info/:id", Auth, nutritionalInfo, recipeDetails);
 
 
 
--- backend/services/fetch_recipe.js
+++ backend/services/fetch_recipe.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+
 const fetchRecipe = async (req, res, next) => {
     const { query, cuisine } = req.body
     var endpoint = "complexSearch"
@@ -23,9 +24,104 @@ const fetchRecipe = async (req, res, next) => {
     }
 };
 
+const recipeDetails = async (req, res, next) => {
+    const { id } = req.params;
+    const query = req.query;
+    const nutritionsNames = req.nutritions;
+
+
+    if (!id) {
+
+        return res.status(400).json({ error: 'Recipe ID is required' });
+    }
+
+    const endpoint = `${id}/information`;
+
+    try {
+
+        const response = await axios.get(`https://api.spoonacular.com/recipes/${endpoint}?apiKey=${process.env.recipe_api_key}`, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            params: {
+                query: query,
+
+            },
+        });
+
+        const recipeData = response.data;
+        const ingredientsNames = [];
+        const instructions = recipeData.instructions
+        const title = recipeData.title
+        const image = recipeData.image
+        const id = recipeData.id
+        const summary = recipeData.summary
+
+        const steps = await recipeData.analyzedInstructions[0].steps;
+        steps.forEach((step) => {
+            if (step.ingredients) {
+
+                step.ingredients.forEach((ingredient) => {
+
+                    ingredientsNames.push(ingredient.name);
+                });
+            }
+        });
+        const allData = { ingredientsNames, instructions, title, image, id, summary, nutritionsNames }
+        res.status(200).json(allData);
+    } catch (error) {
+        console.error('Spoonacular API Error:', error);
+        res.status(500).json({ error: 'An error occurred while fetching recipe data.' });
+    }
+};
+
+
+const nutritionalInfo = async (req, res, next) => {
+    const { id } = req.params;
+    const query = req.query;
+
+
+    if (!id) {
+        return res.status(400).json({ error: 'Recipe ID is required' });
+    }
+
+    const endpoint = `${id}/nutritionWidget.json`;
+
+    try {
+        const response = await axios.get(`https://api.spoonacular.com/recipes/${endpoint}?apiKey=${process.env.recipe_api_key}`, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            params: {
+                query: query,
+
+            },
+        });
+
+        const recipeData = response.data;
+        const nutritionsNames = [];
+
+
+        const nutrition = await recipeData.nutrients;
+        nutrition.forEach((step) => {
+            if (step.name) {
+
+                nutritionsNames.push(step.name);
+            }
+        });
+        req.nutritions = nutritionsNames;
+        next()
+    } catch (error) {
+        console.error('Spoonacular API Error:', error);
+        res.status(500).json({ error: 'An error occurred while fetching recipe data.' });
+    }
+};
+
+
 
 
 module.exports = {
     fetchRecipe,
-   
+    recipeDetails,
+    nutritionalInfo
 };
